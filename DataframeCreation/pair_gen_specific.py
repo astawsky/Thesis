a167ab9bc60c13d09cdf96b689079a84eb4fa202
alestawsky <alestawkier@gmail.com>
2020-11-24 17:49:13 +0200

    First Git

--- DataframeCreation/pair_gen_specific.py
+++ DataframeCreation/pair_gen_specific.py
@@ -10,176 +10,6 @@ sys.path.append(r'/Users/alestawsky/PycharmProjects/Thesis')
 from CustomFuncsAndVars.global_variables import phenotypic_variables, symbols, create_folder, datasets
 
 """ Create the correlation between all pair bacteria across lineages in a dataset """
-
-
-# def generation_specific(bootstraps=False):
-#     # get the three datasets we need to compute the covariance and correlations
-#     physical_units, trace_centered, trace_means_df = pd.read_csv(args.pu), pd.read_csv(args.tc), pd.read_csv(args.ta)
-#
-#     """ Get the covariance and correlation and error from theoretical equality """
-#
-#     # dataframe that we will save
-#     df = pd.DataFrame(
-#         columns=['variable_a', 'variable_b', 'AB_parameter_pair', 'covariance', 'correlation', 'kind', 'average over lineages, trace A',
-#                  'average over lineages, trace B', 'cov difference between data and model' 'dataset', 'generation'])
-#
-#     # dataframe that we will save
-#     bs = pd.DataFrame(
-#         columns=['variable_a', 'variable_b', 'AB_parameter_pair', 'covariance', 'correlation', 'kind', 'average over lineages, trace A',
-#                  'average over lineages, trace B', 'cov difference between data and model', 'dataset', 'generation'])
-#
-#     for dataset in datasets:
-#         # go over all symmetric pairings
-#
-#         print(dataset)
-#
-#         repeats = []
-#         for variable_a in phenotypic_variables:
-#             print(variable_a)
-#             for variable_b in phenotypic_variables:
-#                 # if variable_b not in repeats:
-#                 if variable_b == variable_a:
-#
-#                     for generation in np.arange(6):
-#                         # The dataset mask
-#                         pu_a = physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
-#                             variable_a].reset_index(drop=True)
-#                         pu_b = physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[False, True])[
-#                             variable_b].reset_index(drop=True)
-#
-#                         tc_a = trace_centered[(trace_centered['dataset'] == dataset) & (trace_centered['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
-#                             variable_a].reset_index(drop=True)
-#                         tc_b = trace_centered[(trace_centered['dataset'] == dataset) & (trace_centered['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[False, True])[
-#                             variable_b].reset_index(drop=True)
-#
-#                         ta_a = trace_means_df[(trace_means_df['dataset'] == dataset) & (trace_means_df['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
-#                             variable_a].reset_index(drop=True)
-#                         ta_b = trace_means_df[(trace_means_df['dataset'] == dataset) & (trace_means_df['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[False, True])[
-#                             variable_b].reset_index(drop=True)
-#
-#                         a_std = np.std(pu_a)
-#                         b_std = np.std(pu_b)
-#
-#                         # do it without bootstrap so we have an empirical measurement of the time-averages
-#                         pu_cov = np.cov(pu_a, pu_b)[0, 1]
-#                         ta_cov = np.cov(ta_a, ta_b)[0, 1]
-#                         tc_cov = np.cov(tc_a, tc_b)[0, 1]
-#                         actual_cov = ta_cov + tc_cov
-#
-#                         pu_corr = pu_cov / (a_std * b_std)
-#                         tc_corr = tc_cov / (a_std * b_std)
-#                         ta_corr = ta_cov / (a_std * b_std)
-#                         actual_corr = actual_cov / (a_std * b_std)
-#
-#                         for kind, a, b, cov, corr in [
-#                             ('physical_units', np.mean(pu_a), np.mean(pu_b), pu_cov, pu_corr),
-#                             ('trace_centered', np.mean(tc_a), np.mean(tc_b), tc_cov, tc_corr),
-#                             ('time_averages', np.mean(ta_a), np.mean(ta_b), ta_cov, ta_corr),
-#                             ('model', np.mean(ta_a + tc_a), np.mean(ta_b + tc_b), actual_cov, actual_corr)
-#                         ]:
-#
-#                             # the labels
-#                             if variable_a == variable_b:
-#                                 # It looks nicer for the paper
-#                                 label = '{}'.format(symbols[kind][variable_a])
-#                             else:
-#                                 label = '{}, {}'.format(symbols[kind][variable_a], symbols[kind][variable_b])
-#
-#                             # save it to the dataframe
-#                             df = df.append({
-#                                 'variable_a': variable_a,
-#                                 'variable_b': variable_b,
-#                                 'AB_parameter_pair': label,
-#                                 'covariance': cov,
-#                                 'correlation': corr,
-#                                 'kind': kind,
-#                                 'average over lineages, trace A': a,
-#                                 'average over lineages, trace B': b,
-#                                 'cov difference between data and model': actual_cov - pu_cov,
-#                                 'corr difference between data and model': actual_corr - pu_corr,
-#                                 'dataset': dataset,
-#                                 'generation': generation
-#                             }, ignore_index=True)
-#
-#                         if not bootstraps:
-#                             pass
-#                         else:
-#                             # do the bootstrap
-#                             for _ in np.arange(bootstraps):
-#                                 indices = \
-#                                     physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
-#                                         variable_a].reset_index(
-#                                         drop=True).sample(frac=1, replace=True).index
-#
-#                                 # The dataset mask
-#                                 pu_a = physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
-#                                     variable_a].reset_index(drop=True).iloc[
-#                                     indices]
-#                                 pu_b = \
-#                                     physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[False, True])[
-#                                         variable_b].reset_index(drop=True).iloc[
-#                                         indices]
-#
-#                                 tc_a = trace_centered[(trace_centered['dataset'] == dataset) & (trace_centered['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
-#                                     variable_a].reset_index(drop=True).iloc[
-#                                     indices]
-#                                 tc_b = \
-#                                     trace_centered[(trace_centered['dataset'] == dataset) & (trace_centered['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[False, True])[
-#                                         variable_b].reset_index(drop=True).iloc[
-#                                         indices]
-#
-#                                 ta_a = trace_means_df[(trace_means_df['dataset'] == dataset) & (trace_means_df['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
-#                                     variable_a].reset_index(drop=True).iloc[
-#                                     indices]
-#                                 ta_b = \
-#                                     trace_means_df[(trace_means_df['dataset'] == dataset) & (trace_means_df['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[False, True])[
-#                                         variable_b].reset_index(drop=True).iloc[
-#                                         indices]
-#
-#                                 # do it without bootstrap so we have an empirical measurement of the time-averages
-#                                 pu_cov = np.cov(pu_a, pu_b)[0, 1]  # np.mean([(a - a_mean) * (b - b_mean) for a, b in zip(pu_a, pu_b)])
-#                                 ta_cov = np.cov(ta_a, ta_b)[0, 1]  # np.mean([(a - a_mean) * (b - b_mean) for a, b in zip(ta_a, ta_b)])
-#                                 tc_cov = np.cov(tc_a, tc_b)[0, 1]  # np.mean([a * b for a, b in zip(tc_a, tc_b)])
-#                                 actual_cov = ta_cov + tc_cov
-#
-#                                 pu_corr = pu_cov / (a_std * b_std)
-#                                 tc_corr = tc_cov / (a_std * b_std)
-#                                 ta_corr = ta_cov / (a_std * b_std)
-#                                 actual_corr = actual_cov / (a_std * b_std)
-#
-#                                 for kind, a, b, cov, cor in [
-#                                     ('physical_units', np.mean(pu_a), np.mean(pu_b), pu_cov, pu_corr),
-#                                     ('trace_centered', np.mean(tc_a), np.mean(tc_b), tc_cov, tc_corr),
-#                                     ('time_averages', np.mean(ta_a), np.mean(ta_b), ta_cov, ta_corr),
-#                                     ('model', np.mean(ta_a + tc_a), np.mean(ta_b + tc_b), actual_cov, actual_corr)
-#                                 ]:
-#                                     # the labels
-#                                     if variable_a == variable_b:
-#                                         # It looks nicer for the paper
-#                                         label = '{}'.format(symbols[kind][variable_a])
-#                                     else:
-#                                         label = '{}, {}'.format(symbols[kind][variable_a], symbols[kind][variable_b])
-#
-#                                     # save it to the dataframe
-#                                     bs = bs.append({
-#                                         'variable_a': variable_a,
-#                                         'variable_b': variable_b,
-#                                         'AB_parameter_pair': label,
-#                                         'covariance': cov,
-#                                         'correlation': corr,
-#                                         'kind': kind,
-#                                         'average over lineages, trace A': a,
-#                                         'average over lineages, trace B': b,
-#                                         'cov difference between data and model': actual_cov - pu_cov,
-#                                         'corr difference between data and model': actual_corr - pu_corr,
-#                                         'dataset': dataset,
-#                                         'generation': generation
-#                                     }, ignore_index=True)
-#
-#             # append the parameter so we do not have any repetitions
-#             repeats.append(variable_a)
-#
-#     return [df, bs]
 def generation_specific(bootstraps=False):
     
     """ Get the covariance and correlation and error from theoretical equality """
@@ -203,14 +33,14 @@ def generation_specific(bootstraps=False):
         for variable_a in phenotypic_variables:
             print(variable_a)
             for variable_b in phenotypic_variables:
-                # if variable_b not in repeats:
-                if variable_b == variable_a:
+                if variable_b not in repeats:
+                # if variable_b == variable_a:
                     
                     # An approximation so that we normalize all generations by the same std and more robust that taking it at each generation
-                    a_std = np.std(physical_units[(physical_units['dataset'] == dataset)][variable_a])
-                    b_std = np.std(physical_units[(physical_units['dataset'] == dataset)][variable_b])
+                    a_std = np.mean([np.std(physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == gen)][variable_a]) for gen in np.arange(number_of_generations)])
+                    b_std = np.mean([np.std(physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == gen)][variable_b]) for gen in np.arange(number_of_generations)])
                     
-                    for generation in np.arange(6):
+                    for generation in np.arange(number_of_generations):
                         # The dataset mask
                         pu_a = physical_units[(physical_units['dataset'] == dataset) & (physical_units['generation'] == generation)].sort_values(['trace', 'trap_ID'], ascending=[True, True])[
                             variable_a].reset_index(drop=True)
@@ -228,11 +58,13 @@ def generation_specific(bootstraps=False):
                         tc_cov = pu_cov - ta_cov
                         actual_cov = ta_cov + tc_cov
                         
+                        # Calculate the "correlations"
                         pu_corr = pu_cov / (a_std * b_std)
                         ta_corr = ta_cov / (a_std * b_std)
                         tc_corr = tc_cov / (a_std * b_std)
                         actual_corr = actual_cov / (a_std * b_std)
                         
+                        # Add them to the dataframe we will export
                         for kind, cov, corr in [
                             ('physical_units', pu_cov, pu_corr),
                             ('trace_centered', tc_cov, tc_corr),
@@ -258,7 +90,7 @@ def generation_specific(bootstraps=False):
                                 'cov difference between data and model': actual_cov - pu_cov,
                                 'corr difference between data and model': actual_corr - pu_corr,
                                 'dataset': dataset,
-                                'generation': generation
+                                'generation': generation + 1
                             }, ignore_index=True)
                         
                         if not bootstraps:
@@ -323,7 +155,7 @@ def generation_specific(bootstraps=False):
                                         'cov difference between data and model': actual_cov - pu_cov,
                                         'corr difference between data and model': actual_corr - pu_corr,
                                         'dataset': dataset,
-                                        'generation': generation
+                                        'generation': generation + 1
                                     }, ignore_index=True)
             
             # append the parameter so we do not have any repetitions
@@ -350,6 +182,8 @@ create_folder(args.save_folder)
 
 # get the three datasets we need to compute the covariance and correlations
 physical_units, trace_centered, trace_means_df = pd.read_csv(args.pu), pd.read_csv(args.tc), pd.read_csv(args.ta)
+
+number_of_generations = 6
 
 pair_correlation, bootstrapped = generation_specific(bootstraps=args.bootstraps)
 
--- Plotting/pair_gen_specific_figures.py
+++ Plotting/pair_gen_specific_figures.py
@@ -5,9 +5,80 @@ import os
 import pandas as pd
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
+import seaborn as sns
+from string import ascii_uppercase as uppercase_letters
 
 sys.path.append(r'/Users/alestawsky/PycharmProjects/Thesis')
-from CustomFuncsAndVars.global_variables import phenotypic_variables, symbols, create_folder, datasets
+from CustomFuncsAndVars.global_variables import phenotypic_variables, symbols, create_folder, datasets, seaborn_preamble
+
+
+cmap = sns.color_palette('tab10')[5:8]
+
+
+def plot_panel(var_a, var_b, ax):
+    # The constant
+    df = pair_correlation[(pair_correlation['kind'] == 'time_averages') & (pair_correlation['variable_a'] == var_a) & (pair_correlation['variable_b'] == var_b)].copy()
+    
+    # Plot the constant impact of the time-average in the
+    for count, hue in enumerate(datasets):
+        
+        # first line is for a makeshift border, otherwise it is difficult to see the horizontal lines with the same colors as the barplots
+        ax.axhline(np.unique(df[df['dataset'] == hue]['correlation']), ls='-', color='black', linewidth=2.5)
+        ax.axhline(np.unique(df[df['dataset'] == hue]['correlation']), ls='-', color=cmap[count], linewidth=1)
+    
+    sns.barplot(data=pair_correlation[(pair_correlation['kind'] == 'trace_centered') & (pair_correlation['variable_a'] == var_a) & (pair_correlation['variable_b'] == var_b)], x='generation',
+                y='correlation', hue='dataset', palette=cmap, edgecolor='black', ax=ax)
+    
+    
+def plot_main_figure():
+    seaborn_preamble()
+    fig, axes = plt.subplots(2, 2, tight_layout=True, figsize=[7, 7], sharey=True, sharex=True)
+    axes = axes.flatten()
+    for count, var_a in enumerate(['fold_growth', 'generationtime', 'length_birth', 'growth_rate']):
+        var_b = var_a
+        ax = axes[count]
+        plot_panel(var_a, var_b, ax)
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+        ax.set_title('{}'.format(symbols['physical_units'][var_a]))
+
+        # Let's put the titles
+        ax.set_title(uppercase_letters[count], x=-0.04, fontsize='xx-large')
+        
+        # the legend
+        if count == 0:
+            ax.legend(title='')
+        else:
+            ax.get_legend().remove()
+    
+    plt.savefig('{}/gen_specific_main_figure.png'.format(args.figs_location), dpi=300)
+    # plt.show()
+    plt.close()
+    
+
+def plot_three_in_one():
+    repeats = []
+    for var_a in phenotypic_variables:
+        for var_b in phenotypic_variables:
+            if var_b not in repeats:
+                
+                seaborn_preamble()
+                fig, ax = plt.subplots(1, 1, tight_layout=True, figsize=[7, 7])
+                
+                plot_panel(var_a, var_b, ax)
+                
+                plt.legend()
+                plt.ylabel('')
+                plt.xlabel('')
+                plt.title('{} and {}'.format(symbols['physical_units'][var_a], symbols['physical_units'][var_b]))
+                if var_a == var_b:
+                    plt.savefig('{}/Diagonal gen-specific correlations/{}'.format(args.figs_location, var_a), dpi=300)
+                else:
+                    plt.savefig('{}/Non-Diagonal gen-specific correlations/{}, {}'.format(args.figs_location, var_a, var_b), dpi=300)
+                # plt.show()
+                plt.close()
+        repeats.append(var_a)
 
 
 parser = argparse.ArgumentParser(description='Dataframes containing: KL divergences, Population physical_units lineages, and the ergodicity breaking parameter for both kinds of lineages.')
@@ -19,19 +90,24 @@ parser.add_argument('-ta', '--ta', metavar='', type=str, help='What to name the 
                     required=False, default=os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/Data/time_averages_with_control.csv')
 parser.add_argument('-tc', '--tc', metavar='', type=str, help='What to name the trace-centered dataframe with control added',
                     required=False, default=os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/Data/trace_centered_with_control.csv')
-parser.add_argument('-bs', '--bootstraps', metavar='', type=int, help='How many bootstraps per covariance should be done?',
-                    required=False, default=0)
+parser.add_argument('-f', '--figs_location', metavar='', type=str, help='Where the figures are saved.',
+                    required=False, default=os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/Figures')
+
 
 args = parser.parse_args()
 
+
 create_folder(args.save_folder)
+create_folder(args.figs_location)
+create_folder(args.figs_location+'/Diagonal gen-specific correlations')
+create_folder(args.figs_location+'/Non-Diagonal gen-specific correlations')
+
 
 # save it to the Data folder
 pair_correlation = pd.read_csv('{}/over_lineages.csv'.format(args.save_folder))
 
-print(pair_correlation)
-print(pair_correlation.columns)
 
-for var_a in phenotypic_variables:
-    for var_b in phenotypic_variables:
+plot_main_figure()
+
+plot_three_in_one()
 
