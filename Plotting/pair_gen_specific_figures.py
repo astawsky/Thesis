b0c778b4661f5e1e04835903640e80034cc67b49
alestawsky <alestawkier@gmail.com>
2020-11-25 09:44:35 +0200

    First Git

--- Plotting/pair_gen_specific_figures.py
+++ Plotting/pair_gen_specific_figures.py
@@ -14,43 +14,47 @@ from CustomFuncsAndVars.global_variables import phenotypic_variables, symbols, c
 
 
 cmap = sns.color_palette('tab10')[5:8]
+cmap_dark = sns.color_palette('pastel')[5:8]
 
 
-def plot_panel(var_a, var_b, ax):
+def plot_panel(var_a, var_b, count1, ax):
     # The constant
     df = pair_correlation[(pair_correlation['kind'] == 'time_averages') & (pair_correlation['variable_a'] == var_a) & (pair_correlation['variable_b'] == var_b)].copy()
+
+    sns.barplot(data=pair_correlation[(pair_correlation['kind'] == 'trace_centered') & (pair_correlation['variable_a'] == var_a) & (pair_correlation['variable_b'] == var_b)], x='generation',
+                y='correlation', hue='dataset', palette=cmap, edgecolor='black', ax=ax)
+    
+    ax.get_legend().remove()
     
     # Plot the constant impact of the time-average in the
     for count, hue in enumerate(datasets):
         
         # first line is for a makeshift border, otherwise it is difficult to see the horizontal lines with the same colors as the barplots
-        ax.axhline(np.unique(df[df['dataset'] == hue]['correlation']), ls='-', color='black', linewidth=2.5)
-        ax.axhline(np.unique(df[df['dataset'] == hue]['correlation']), ls='-', color=cmap[count], linewidth=1)
-    
-    sns.barplot(data=pair_correlation[(pair_correlation['kind'] == 'trace_centered') & (pair_correlation['variable_a'] == var_a) & (pair_correlation['variable_b'] == var_b)], x='generation',
-                y='correlation', hue='dataset', palette=cmap, edgecolor='black', ax=ax)
+        # ax.axhline(np.unique(df[df['dataset'] == hue]['correlation'])+.01, ls='--', color='black', linewidth=2)
+        # ax.axhline(np.unique(df[df['dataset'] == hue]['correlation'])-.01, ls='--', color='black', linewidth=2)
+        if count1 == 1:
+            ax.axhline(np.unique(df[df['dataset'] == hue]['correlation']), ls='--', color=cmap_dark[count], linewidth=3, label=hue)
+        else:
+            ax.axhline(np.unique(df[df['dataset'] == hue]['correlation']), ls='--', color=cmap_dark[count], linewidth=3)
     
     
 def plot_main_figure():
     seaborn_preamble()
     fig, axes = plt.subplots(2, 2, tight_layout=True, figsize=[7, 7], sharey=True, sharex=True)
     axes = axes.flatten()
-    for count, var_a in enumerate(['fold_growth', 'generationtime', 'length_birth', 'growth_rate']):
+    for count1, var_a in enumerate(['fold_growth', 'generationtime', 'length_birth', 'growth_rate']):
         var_b = var_a
-        ax = axes[count]
-        plot_panel(var_a, var_b, ax)
+        ax = axes[count1]
+        plot_panel(var_a, var_b, count1, ax)
         ax.set_xlabel('')
-        ax.set_ylabel('')
-        ax.set_title('{}'.format(symbols['physical_units'][var_a]))
+        ax.set_ylabel(symbols['trace_centered'][var_a], fontsize='xx-large')
 
         # Let's put the titles
-        ax.set_title(uppercase_letters[count], x=-0.04, fontsize='xx-large')
+        ax.set_title(uppercase_letters[count1], x=-0.07, fontsize='xx-large')
         
         # the legend
-        if count == 0:
+        if count1 in [0, 1]:
             ax.legend(title='')
-        else:
-            ax.get_legend().remove()
     
     plt.savefig('{}/gen_specific_main_figure.png'.format(args.figs_location), dpi=300)
     # plt.show()
@@ -108,5 +112,7 @@ pair_correlation = pd.read_csv('{}/over_lineages.csv'.format(args.save_folder))
 
 
 plot_main_figure()
+
+exit()
 
 plot_three_in_one()
